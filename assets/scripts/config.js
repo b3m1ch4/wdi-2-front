136a3bbfd627de54e5d73f9f429d5b9a87cc5898
b3m1ch4 <b3.m1ch4@gmail.com>
2018-08-31 11:22:35 -0400

    Structure
    Created file structure to code base for front end

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: '<https://quiet-castle-23974.herokuapp.com/>',
   development: 'http://localhost:4741'
 }
 
