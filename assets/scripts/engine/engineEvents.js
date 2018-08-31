fdab85e61c33d7d4ff0d522aeca4d9bc2582ba6c
b3m1ch4 <b3.m1ch4@gmail.com>
2018-08-31 16:46:19 -0400

    Styling
    Style the front end app with bootstrap 4

--- assets/scripts/engine/engineEvents.js
+++ assets/scripts/engine/engineEvents.js
@@ -4,6 +4,48 @@ let engineApi = require('./engineApi.js')
 let engineUi = require('./engineUi.js')
 let store = require('../store.js')
 
+//
+const onAllGames = function (event) {
+  event.preventDefault()
+  gameApi.allGames()
+    .then(gameUi.indexSuccess)
+    .catch(gameUi.apiFail)
+}
+//
+// const onFindGame = function (event) {
+//   event.preventDefault()
+//   const id = $('#find-game input').val()
+//   gameApi.oneGame(id)
+//   .then(gameUi.findGame)
+//   .catch(gameUi.apiFail)
+// }
+// onNewGame creates a new game in the Api
+// let onNewGig = function (event) {
+//   event.preventDefault()
+//   const data = getFormFields(event.target)
+//   gameApi.newGame(data)
+//   .then(gameUi.gameStart)
+//   .catch(gameUi.apiFail)
+// }
+// onPatchGame updates the api with the gameBoard's info
+// let onPatchGame = function (event) {
+//   event.preventDefault()
+//   let i = event.target.id.slice(0, 1)
+//   const data = {
+//   "gig": {
+//     "cell": {
+//       "index": i,
+//       "value": gameLogic.current.player
+//     },
+//     "cancelled": false
+//   }
+// }
+//   gameApi.patchGame(data)
+//   .then(gameUi.apiUpdate)
+//   .catch(gameUi.apiFail)
+// }
+//
+
 // event listeners
 let engineHandlers = function () {
 }
--- assets/scripts/engine/engineUi.js
+++ assets/scripts/engine/engineUi.js
@@ -1,9 +1,23 @@
 'use strict'
 /* ===== required files ===== */
 let store = require('../store')
-
+//
+const indexSuccess = function (response) {
+  response.games.forEach((game) => {
+  let allGigs = response.gigs.length
+  $('#game-message').html("all " + allGigs + "!")
+  })
+}
+//
+const apiFail = function (response) {
+    response.preventDefault()
+    $('#user-message').html('<p>API Fail. Please try again.</p>>')
+}
+//
 const apiUpdate = function (response) {
   store = response
 }
 //
-module.exports = {}
+module.exports = {
+  indexSuccess
+}
