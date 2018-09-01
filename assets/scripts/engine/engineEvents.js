0c62b19e840cbae8c555184bd38382cac1d4ea5f
b3m1ch4 <b3.m1ch4@gmail.com>
2018-09-01 12:37:46 -0400

    Index event
    User can index all gig events

--- assets/scripts/engine/engineEvents.js
+++ assets/scripts/engine/engineEvents.js
@@ -4,8 +4,54 @@ let engineApi = require('./engineApi.js')
 let engineUi = require('./engineUi.js')
 let store = require('../store.js')
 
+<<<<<<< Updated upstream
+=======
+//
+const onAllGigs = function (event) {
+  event.preventDefault()
+  engineApi.allGigs()
+    .then(engineUi.indexSuccess)
+    .catch(engineUi.apiFail)
+}
+//
+// const onFindGig = function (event) {
+//   event.preventDefault()
+//   const id = $('#find-gig input').val()
+//   gigApi.oneGig(id)
+//   .then(gigUi.findGig)
+//   .catch(gigUi.apiFail)
+// }
+// onNewGig creates a new gig in the Api
+// let onNewGig = function (event) {
+//   event.preventDefault()
+//   const data = getFormFields(event.target)
+//   gigApi.newGig(data)
+//   .then(gigUi.gigStart)
+//   .catch(gigUi.apiFail)
+// }
+// onPatchGig updates the api with the gigBoard's info
+// let onPatchGig = function (event) {
+//   event.preventDefault()
+//   let i = event.target.id.slice(0, 1)
+//   const data = {
+//   "gig": {
+//     "cell": {
+//       "index": i,
+//       "value": gigLogic.current.player
+//     },
+//     "cancelled": false
+//   }
+// }
+//   gigApi.patchGig(data)
+//   .then(gigUi.apiUpdate)
+//   .catch(gigUi.apiFail)
+// }
+//
+
+>>>>>>> Stashed changes
 // event listeners
 let engineHandlers = function () {
+  $('#gig-index').on('click', onAllGigs)
 }
 //
 module.exports = {
--- assets/scripts/engine/engineUi.js
+++ assets/scripts/engine/engineUi.js
@@ -1,7 +1,27 @@
 'use strict'
 /* ===== required files ===== */
 let store = require('../store')
+<<<<<<< Updated upstream
 
+=======
+//
+const apiFail = function (response) {
+    response.preventDefault()
+    $('#user-message').html('<p>API Fail. Please try again.</p>>')
+}
+//
+const indexSuccess = function (response) {
+  console.log(response)
+  response.gigs.forEach((gig) => {
+  let gigId = gig.id
+  console.log(gigId)
+  $('#gigs-display').append("gigId is" + gigId)
+  })
+}
+//
+
+//
+>>>>>>> Stashed changes
 const apiUpdate = function (response) {
   store = response
 }
