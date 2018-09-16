4ead4836b12855d189a6e56869e7c8cb093da2d6
b3m1ch4 <b3.m1ch4@gmail.com>
2018-09-16 17:48:25 -0400

    Test 2
    Deploy test 2

--- assets/scripts/engine/engineEvents.js
+++ assets/scripts/engine/engineEvents.js
@@ -11,7 +11,6 @@ const onNewGig = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   if (data.gig.title !== '' && data.gig.description !== '') {
-    console.log(data.gig.title)
     engineApi.newGig(data)
     .then(engineUi.createSuccess)
     .catch(engineUi.apiFail)
